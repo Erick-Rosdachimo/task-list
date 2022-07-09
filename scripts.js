7a49041aa735031e5d05831848fe48a72db8ec0f
Erick Rosdachimo <erickrosdachimoribeiro@gmail.com>
2022-07-09 17:09:22 -0300

    Lista de tarefa salvando cores e tasks

--- scripts.js
+++ scripts.js
@@ -1,110 +1,143 @@
 // AO CARREGAR A PAGINA
 window.addEventListener('load', () => {
-  let arrayElements = localStorage.localS.split('<%')
+  let arrayElements
   let tasksContadas = 1
 
-  for (const element of arrayElements) {
-    if (tasksContadas == arrayElements.length) {
-    } else {
-      tasksContadas = tasksContadas + 1
-      // Todas as tasks
-      const task = document.getElementsByClassName('task')
+  if (localStorage.localS == undefined) {
+  } else {
+    arrayElements = localStorage.localS.split('<%')
 
-      // criando os elementos
-      const novaTask = document.createElement('div')
-      const novaTaskP = document.createElement('p')
-      const novaTaskSpan = document.createElement('span')
-      const novaTaskI1 = document.createElement('i')
-      const novaTaskI2 = document.createElement('i')
-
-      // nomeando os elementos
-      novaTask.classList.add('task')
-      novaTaskI1.classList.add('icon-ok')
-      novaTaskI2.classList.add('icon-trashcan')
-
-      // Adicionando o EventListener com as funções para deletar e completar as tarefas
-      novaTaskI1.addEventListener('click', () => complete(novaTask, novaTaskP))
-      novaTaskI2.addEventListener('click', () =>
-        deletarTarefa(novaTask, novaTaskP)
-      )
-
-      // colocando o texto
-      if (localStorage.localS.search(element + '<%/') >= 0) {
-        novaTask.classList.add('complete')
-        novaTask.style.backgroundColor = '#4b4b4b'
-        if (element[0] == '/') {
-          novaTaskP.textContent = element.slice(1)
-        } else {
-          novaTaskP.textContent = element
-        }
+    for (const element of arrayElements) {
+      if (tasksContadas == arrayElements.length) {
       } else {
-        if (element[0] == '/') {
-          novaTaskP.textContent = element.slice(1)
-        } else {
-          novaTaskP.textContent = element
-        }
-      }
-
-      //posicionando os elementos
-      boxTasks.appendChild(novaTask)
-      task[task.length - 1].appendChild(novaTaskP)
-      task[task.length - 1].appendChild(novaTaskSpan)
-      novaTaskSpan.appendChild(novaTaskI1)
-      novaTaskSpan.appendChild(novaTaskI2)
-
-      // Deletar as tarefas
-      const deletarTarefa = (novaTask, novaTaskP) => {
+        tasksContadas = tasksContadas + 1
         // Todas as tasks
-        const tasks = boxTasks.childNodes
-        for (const task of tasks) {
-          const currentTaskIsBeingClicked =
-            task.firstChild.isSameNode(novaTaskP)
-          if (currentTaskIsBeingClicked) {
-            let splitOne
-            if (localStorage.localS.search(novaTaskP.innerHTML + '<%/') >= 0) {
-              splitOne = localStorage.localS.split(novaTaskP.innerHTML + '<%/')
-            } else {
-              splitOne = localStorage.localS.split(novaTaskP.innerHTML + '<%')
-            }
-            localStorage.localS = splitOne[0] + splitOne[1]
-            console.log(localStorage.localS)
-            novaTask.remove()
+        const task = document.getElementsByClassName('task')
+
+        // criando os elementos
+        const novaTask = document.createElement('div')
+        const novaTaskP = document.createElement('p')
+        const novaTaskSpan = document.createElement('span')
+        const novaTaskI1 = document.createElement('i')
+        const novaTaskI2 = document.createElement('i')
+
+        // nomeando os elementos
+        novaTask.classList.add('task')
+        novaTaskI1.classList.add('icon-ok')
+        novaTaskI2.classList.add('icon-trashcan')
+
+        // Adicionando o EventListener com as funções para deletar e completar as tarefas
+        novaTaskI1.addEventListener('click', () =>
+          complete(novaTask, novaTaskP)
+        )
+        novaTaskI2.addEventListener('click', () =>
+          deletarTarefa(novaTask, novaTaskP)
+        )
+
+        // colocando o texto
+        if (localStorage.localS.search(element + '<%/') >= 0) {
+          novaTask.classList.add('complete')
+          novaTask.style.backgroundColor = '#4b4b4b'
+          if (element[0] == '/') {
+            novaTaskP.textContent = element.slice(1)
+          } else {
+            novaTaskP.textContent = element
+          }
+        } else {
+          if (element[0] == '/') {
+            novaTaskP.textContent = element.slice(1)
+          } else {
+            novaTaskP.textContent = element
           }
         }
-      }
 
-      // Completar as tarefas
-      const complete = (novaTask, novaTaskP) => {
-        const tasks = boxTasks.childNodes
-        for (const task of tasks) {
-          const iconClicked = task.firstChild.isSameNode(novaTaskP)
-          if (iconClicked) {
-            if (localStorage.localS.search(novaTaskP.innerHTML + '<%/') >= 0) {
-              localStorage.localS = localStorage.localS.replace(
-                novaTaskP.innerHTML + '<%/',
-                novaTaskP.innerHTML + '<%'
-              )
-            } else {
-              localStorage.localS = localStorage.localS.replace(
-                novaTaskP.innerHTML + '<%',
-                novaTaskP.innerHTML + '<%/'
-              )
-            }
+        //posicionando os elementos
+        boxTasks.appendChild(novaTask)
+        task[task.length - 1].appendChild(novaTaskP)
+        task[task.length - 1].appendChild(novaTaskSpan)
+        novaTaskSpan.appendChild(novaTaskI1)
+        novaTaskSpan.appendChild(novaTaskI2)
 
-            novaTask.classList.toggle('complete')
-            if (novaTask.classList.contains('complete')) {
-              novaTask.style.backgroundColor = '#4b4b4b'
-            } else {
-              changeTaskAndButtons()
+        // Deletar as tarefas
+        const deletarTarefa = (novaTask, novaTaskP) => {
+          // Todas as tasks
+          const tasks = boxTasks.childNodes
+          for (const task of tasks) {
+            const currentTaskIsBeingClicked =
+              task.firstChild.isSameNode(novaTaskP)
+            if (currentTaskIsBeingClicked) {
+              let splitOne
+              if (
+                localStorage.localS.search(novaTaskP.innerHTML + '<%/') >= 0
+              ) {
+                splitOne = localStorage.localS.split(
+                  novaTaskP.innerHTML + '<%/'
+                )
+              } else {
+                splitOne = localStorage.localS.split(novaTaskP.innerHTML + '<%')
+              }
+              localStorage.localS = splitOne[0] + splitOne[1]
+              console.log(localStorage.localS)
+              novaTask.remove()
             }
           }
         }
-      }
 
-      // Finalizando
-      input.focus()
+        // Completar as tarefas
+        const complete = (novaTask, novaTaskP) => {
+          const tasks = boxTasks.childNodes
+          for (const task of tasks) {
+            const iconClicked = task.firstChild.isSameNode(novaTaskP)
+            if (iconClicked) {
+              if (
+                localStorage.localS.search(novaTaskP.innerHTML + '<%/') >= 0
+              ) {
+                localStorage.localS = localStorage.localS.replace(
+                  novaTaskP.innerHTML + '<%/',
+                  novaTaskP.innerHTML + '<%'
+                )
+              } else {
+                localStorage.localS = localStorage.localS.replace(
+                  novaTaskP.innerHTML + '<%',
+                  novaTaskP.innerHTML + '<%/'
+                )
+              }
+
+              novaTask.classList.toggle('complete')
+              if (novaTask.classList.contains('complete')) {
+                novaTask.style.backgroundColor = '#4b4b4b'
+              } else {
+                changeTaskAndButtons()
+              }
+            }
+          }
+        }
+
+        // Finalizando
+        input.focus()
+      }
     }
   }
+  // definindo
+  document.querySelector('button.add').style.backgroundColor =
+    localStorage.corTask
+  document.querySelector('.changeTheme').style.backgroundColor =
+    localStorage.corTask
+  document.querySelector('.save').style.backgroundColor = localStorage.corTask
+
+  let tasksIncompletas = document.querySelectorAll('.task:not(.complete)')
+  for (const task of tasksIncompletas) {
+    task.style.backgroundColor = localStorage.corTask
+  }
+  if (localStorage.corTask == undefined) {
+  } else {
+    document.getElementById('tasksAndButtons').value = localStorage.corTask
+  }
+  document.querySelector('footer').style.backgroundColor =
+    localStorage.corBackground
+  document.querySelector('body').style.backgroundColor =
+    localStorage.corBackground
+  document.getElementById('background').value = localStorage.corBackground
 })
 
 const input = document.getElementById('input')
@@ -234,53 +267,29 @@ function fechaMudarCor() {
 close.addEventListener('click', fechaMudarCor)
 open.addEventListener('click', abreMudarCor)
 
-let mexeu = false
-
 function changeTaskAndButtons() {
-  var buttonAdd = document.querySelector('button.add')
-  var tasks = document.querySelectorAll('.task:not(.complete)')
-  var changeBtn = document.querySelector('.changeTheme')
-  var saveBtn = document.querySelector('.save')
-  if (mexeu) {
-    buttonAdd.style.backgroundColor = valueBtn
-    changeBtn.style.backgroundColor = valueBtn
-    saveBtn.style.backgroundColor = valueBtn
+  //value
+  let valueBtn = document.getElementById('tasksAndButtons').value
 
-    for (const task of tasks) {
-      task.style.backgroundColor = valueBtn
-    }
-  } else {
-    tasksAndButtons()
-  }
-}
+  let tasksIncompletas = document.querySelectorAll('.task:not(.complete)')
 
-function tasksAndButtons() {
-  var valueTaskButton = document.getElementById('tasksAndButtons').value
-  var buttonAdd = document.querySelector('button.add')
-  var tasks = document.querySelectorAll('.task:not(.complete)')
-  var changeBtn = document.querySelector('.changeTheme')
-  var saveBtn = document.querySelector('.save')
-  valueBtn = valueTaskButton
+  // definindo
+  document.querySelector('button.add').style.backgroundColor = valueBtn
+  document.querySelector('.changeTheme').style.backgroundColor = valueBtn
+  document.querySelector('.save').style.backgroundColor = valueBtn
 
-  buttonAdd.style.backgroundColor = valueTaskButton
-  changeBtn.style.backgroundColor = valueTaskButton
-  saveBtn.style.backgroundColor = valueTaskButton
+  localStorage.corTask = valueBtn
 
-  for (const task of tasks) {
-    task.style.backgroundColor = valueTaskButton
+  for (const task of tasksIncompletas) {
+    task.style.backgroundColor = valueBtn
   }
 }
 
 function changeAction() {
-  var valueBackground = document.getElementById('background').value
-
-  var body = document.getElementById('body')
-  let footer = document.getElementById('footer')
-
-  body.style.backgroundColor = valueBackground
-  footer.style.backgroundColor = valueBackground
-
-  mexeu = true
-  tasksAndButtons()
+  let valueBackground = document.getElementById('background').value
+  document.querySelector('body').style.backgroundColor = valueBackground
+  document.querySelector('footer').style.backgroundColor = valueBackground
+  localStorage.corBackground = valueBackground
+  changeTaskAndButtons()
   fechaMudarCor()
 }
